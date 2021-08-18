487e97ea88f7f4037992dc3f5ebc613b126168d2
Grzegorz <grzegorztarkowski8@wp.pl>
2021-08-18 20:58:45 +0200

    After Episode 32

--- screens/ReviewForm.js
+++ screens/ReviewForm.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { StyleSheet, Button, View, TextInput, Text } from 'react-native';
 import { GlobalStyles } from '../styles/GlobalStyles';
 import { Formik } from 'formik';
+import * as yup from 'yup';
+
+const reviewSchema = yup.object({
+  title: yup.string().required().min(4),
+  body: yup.string().required().min(10),
+  rating: yup
+    .string()
+    .required()
+    .test(
+      'is-num-1-5',
+      'Rating must be a number 1 - 5',
+      (value) => parseInt(value) < 6 && parseInt(value) > 0
+    ),
+});
 
 const ReviewForm = ({ addReview }) => {
   return (
     <View style={GlobalStyles.container}>
       <Formik
         initialValues={{ title: '', body: '', rating: '' }}
+        validationSchema={reviewSchema}
         onSubmit={(values, actions) => {
           addReview(values);
           actions.resetForm();
